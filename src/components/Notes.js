a988563df32c0f0e1aeb6cef4d3fed66bee2e771
alucadeil <alucadeil@mail.ru>
2021-03-01 22:36:32 +0300

    Added animation

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,34 +1,41 @@
 import React, {useContext} from 'react'
 import {FirebaseContext} from '../context/firebase/fireBaseContext'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import {AlertContext} from '../context/alert/alertContext'
 
 export const Notes = ({notes}) => {
   const alert = useContext(AlertContext)
   const firebase = useContext(FirebaseContext)
   return (
-    <ul className="list-group">
+    <TransitionGroup component="ul" className="list-group">
       {notes.map(note =>
-        <li
-          className="list-group-item note"
+        <CSSTransition
           key={note.id}
+          classNames={'note'}
+          timeout={600}
         >
-          <div>
-            <strong>{note.title}</strong>
-            <small>{note.date}</small>
-           </div>
-          <button
-            type="button"
-            className="btn btn-outline-danger btn-sm"
-            onClick={() => {
-              firebase.removeNote(note.id)
-                .then(() => alert.show('Заметка была удалена', 'success'),
-                  () => alert.show('Ошибка при удалении', 'danger'))
-            }}
+          <li
+            className="list-group-item note"
+            
           >
-            &times;
-          </button>
-        </li>
+            <div>
+              <strong>{note.title}</strong>
+              <small>{note.date}</small>
+             </div>
+            <button
+              type="button"
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => {
+                firebase.removeNote(note.id)
+                  .then(() => alert.show('Заметка была удалена', 'success'),
+                    () => alert.show('Ошибка при удалении', 'danger'))
+              }}
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
       )}
-    </ul>
+    </TransitionGroup>
   )
 }
